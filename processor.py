fe43da415b95b195cce7405d7c84ca29b0e772d3
sifal <sifalklioui@yahoo.com>
2024-11-15 21:49:51 +0100

    add: tokenizer details

--- processor.py
+++ processor.py
@@ -8,4 +8,24 @@ IMAGENET_STD = [0.5, 0.5, 0.5]
 class PaliGemmaProcessor:
     def __init__(self, tokenizer, num_image_tokens: int, image_size:int):
 
+        self.image_seq_lenght = num_image_tokens
+        self.image_size = image_size
+
+        tokens_to_add = {"additional_special_tokens": {self.IMAGE_TOKEN}}
+        tokenizer.add_special_tokens(tokens_to_add)
+
+        EXTRA_TOKENS = [
+            f"<loc{i:04d}>" for i in range(1024)
+            ]
+        EXTRA_TOKENS += [
+            f"<seg{i:03d}" for i in range(128)
+        ]
+
+        tokenizer.add_special_tokens(EXTRA_TOKENS)
+
+        self.image_token_id = tokenizer.convert_tokens_to_ids(self.IMAGE_TOKEN)
+
+        tokenizer.add_bos_token = False
+        tokenizer.add_eos_token = False
+        self.tokenizer = tokenizer
         
