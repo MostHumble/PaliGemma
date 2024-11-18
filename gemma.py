a79db2a35503e591435aad5d363c5146c11617d8
sifal <sifalklioui@yahoo.com>
2024-11-18 23:14:41 +0100

    merge simplifed

--- gemma.py
+++ gemma.py
@@ -5,7 +5,72 @@ import torch
 import torch.nn as nn
 from siglip import  SiglipVisionConfig, SiglipVisionModel
 
+class GemmaConfig():
 
+    def __init__(
+        self,
+        vocab_size,
+        hidden_size,
+        intermediate_size,
+        num_hidden_layers,
+        num_attention_heads,
+        num_key_value_heads,
+        head_dim=256,
+        max_position_embeddings=8192,
+        rms_norm_eps=1e-6,
+        rope_theta=10000.0,
+        attention_bias=False,
+        attention_dropout=0.0,
+        pad_token_id=None,
+        **kwargs,
+    ):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.max_position_embeddings = max_position_embeddings
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.head_dim = head_dim
+        self.num_key_value_heads = num_key_value_heads
+        self.rms_norm_eps = rms_norm_eps
+        self.rope_theta = rope_theta
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+        self.pad_token_id = pad_token_id
+
+class PaliGemmaConfig():
+
+    def __init__(
+        self,
+        vision_config=None,
+        text_config=None,
+        ignore_index=-100,
+        image_token_index=256000,
+        vocab_size=257152,
+        projection_dim=2048,
+        hidden_size=2048,
+        pad_token_id=None,
+        **kwargs,
+    ):
+        self.ignore_index = ignore_index
+        self.image_token_index = image_token_index
+        self.vocab_size = vocab_size
+        self.projection_dim = projection_dim
+        self.hidden_size = hidden_size
+        self.vision_config = vision_config
+        self.is_encoder_decoder = False
+        self.pad_token_id = pad_token_id
+
+        self.vision_config = SiglipVisionConfig(**vision_config)
+        self.text_config = text_config
+
+        self.text_config = GemmaConfig(**text_config, pad_token_id=pad_token_id)
+        self.vocab_size = self.text_config.vocab_size
+
+        self.text_config.num_image_tokens = (self.vision_config.image_size // self.vision_config.patch_size) ** 2
+        self.vision_config.projection_dim = projection_dim
+         
 
 class PaliGemmaForConditonnalGeneration(nn.Module):
     def __init__(self, config: PaliGemmaConfig):
@@ -20,6 +85,24 @@ class PaliGemmaForConditonnalGeneration(nn.Module):
 
     def tie_weights(self):
         return self.language_model.tie_weights()
+    
+    def _merge_input_ids_with_image_features(
+        self,
+        image_features: torch.Tensor, 
+        input_embeds: torch.Tensor, 
+        input_ids: torch.Tensor, 
+        attention_mask: torch.Tensor, 
+        kv_cache: Optional[KVCache] = None
+    ):
+        embed_dim = image_features.size(-1)
+        bs, seq_len = input_ids.shape
+        dtype, device = input_embeds.dtype, input_embeds.device
+        # bs, seq_len, hidden_dim
+        scaled_image_features = image_features / (self.config.hidden_size**0.5)
+
+        # placeholder for the embeddings
+        final_embedding = torch.cat([scaled_image_features, input_embeds[:,scaled_image_features.size(1):]], dim=1)
+        
     
     def forward(
             self,
