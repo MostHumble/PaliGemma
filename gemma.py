16272ea216258000d69963d5bf76ac1be44a2c04
sifal <sifalklioui@yahoo.com>
2024-11-25 23:22:19 +0100

    adding forward pass

--- gemma.py
+++ gemma.py
@@ -81,7 +81,49 @@ class PaliGemmaMultiModalProjector(nn.Module):
         hidden_states = self.linear(image_features)
         return hidden_states
     
+class GemmaModel(nn.Module):
 
+    def __init__(self, config: GemmaConfig):
+        super().__init__()
+        self.config = config
+        self.padding_idx = config.pad_token_id
+        self.vocab_size = config.vocab_size
+
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        self.layers = nn.ModuleList(
+            [GemmaDecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
+        )
+        self.norm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def get_input_embeddings(self):
+        return self.embed_tokens
+
+    # Ignore copy
+    def forward(
+        self,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> torch.FloatTensor:
+        
+        # bsz, seq_len, hidden_dim
+        normalizer = torch.tensor(self.config.hidden_size**0.5, dtype=inputs_embeds.dtype)
+        inputs_embeds = inputs_embeds * normalizer
+
+        for decoder_layer in self.layers:
+            # bsz, seq_len, hidden_dim
+            inputs_embeds = decoder_layer(
+                inputs_embeds,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                kv_cache=kv_cache,
+            )
+
+        inputs_embeds = self.norm(inputs_embeds)
+
+        return inputs_embeds
+    
 class GemmaForCausalLM(nn.Module):
 
     def __init__(self, config):
